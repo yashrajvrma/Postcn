0878102560f3d149283eee633c10a84c645c81b4
yashrajvrma <vermayashraj2005@gmail.com>
2025-09-28 12:33:42 +0530

    add theme

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,13 @@
-import { AppSidebar } from "@/components/app-sidebar";
-import { SiteHeader } from "@/components/site-header";
-import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sidebar"
+import { SiteHeader } from "@/components/site-header"
+import {
+  SidebarInset,
+  SidebarProvider,
+} from "@/components/ui/sidebar"
 
-export const iframeHeight = "800px";
+export const iframeHeight = "800px"
 
-export const description = "A sidebar with a header and a search form.";
+export const description = "A sidebar with a header and a search form."
 
 export default function Page() {
   return (
@@ -26,5 +29,5 @@ export default function Page() {
         </div>
       </SidebarProvider>
     </div>
-  );
+  )
 }
