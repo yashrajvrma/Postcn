8c32455ae5a57fa50af6d7cfd87b05f8583d82f3
yashrajvrma <vermayashraj2005@gmail.com>
2025-10-08 21:26:50 +0530

    fixed create collection

--- app/api/collection/create/route.ts
+++ app/api/collection/create/route.ts
@@ -1,8 +1,19 @@
 import prisma from "@/db";
+import { auth } from "@/lib/auth/auth-server";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
 export async function POST(request: Request) {
   try {
     const { name } = await request.json();
+
+    const session = await auth.api.getSession({
+      headers: await headers(),
+    });
+
+    if (!session) {
+      redirect("/signin");
+    }
 
     console.log("collection name is", name);
 
@@ -12,9 +23,13 @@ export async function POST(request: Request) {
       });
     }
 
+    const userId = session.user.id;
+
     const collection = await prisma.collection.create({
       data: {
         name,
+        userId,
+        type: "FOLDER",
       },
     });
 
